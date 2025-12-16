2a2ad9043c728c21fe821931f1f39d88d1199a54
statistikkesra3201-pixel <statistikkesra3201@gmail.com>
2025-12-16 14:08:43 +0700

    Update pengeluaran.js

--- data/pengeluaran.js
+++ data/pengeluaran.js
@@ -1,23 +1,23 @@
-import { v4 as uuid } from "uuidv4";
+import { uuid } from "uuidv4";
 export const data = [
   {
     _id: 1,
-    kategori: "Imunisasi",
+    kategori: "Imunisasi (Rumah Vaksin)",
     nama: "Hepatitis B",
-    biaya: 120000,
+    biaya: 250000,
     is_created_by_user: false,
   },
   {
     _id: 2,
-    kategori: "Imunisasi",
+    kategori: "Imunisasi (Rumah Vaksin)",
     nama: "BCG",
-    biaya: 375000,
+    biaya: 350000,
 
     is_created_by_user: false,
   },
   {
     _id: 3,
-    kategori: "Imunisasi",
+    kategori: "Imunisasi (Rumah Vaksin)",
     nama: "Polio Tetes",
     biaya: 125000,
 
@@ -25,15 +25,15 @@ export const data = [
   },
   {
     _id: 4,
-    kategori: "Imunisasi",
+    kategori: "Imunisasi (Rumah Vaksin)",
     nama: "Polio Suntik (IPV)",
-    biaya: 300000,
+    biaya: 350000,
 
     is_created_by_user: false,
   },
   {
     _id: 5,
-    kategori: "Imunisasi",
+    kategori: "Imunisasi (Rumah Vaksin)",
     nama: "DPT Hib Hb",
     biaya: 350000,
 
@@ -41,17 +41,65 @@ export const data = [
   },
   {
     _id: 6,
-    kategori: "Imunisasi",
-    nama: "Campak Rubella",
-    biaya: 155000,
+    kategori: "Imunisasi (Rumah Vaksin)",
+    nama: "Campak",
+    biaya: 300000,
 
     is_created_by_user: false,
   },
   {
     _id: 7,
-    kategori: "Imunisasi",
+    kategori: "Imunisasi (Rumah Vaksin)",
     nama: "MMR",
-    biaya: 475000,
+    biaya: 550000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 27,
+    kategori: "Imunisasi (Rumah Vaksin)",
+    nama: "Japanese Enchepalitis (JE)",
+    biaya: 450000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 28,
+    kategori: "Imunisasi (Rumah Vaksin)",
+    nama: "Cacar/Varicella",
+    biaya: 711.300,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 29,
+    kategori: "Imunisasi (Rumah Vaksin)",
+    nama: "PCV",
+    biaya: 720000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 30,
+    kategori: "Imunisasi (Rumah Vaksin)",
+    nama: "Hepatitis A",
+    biaya: 450000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 31,
+    kategori: "Imunisasi (Rumah Vaksin)",
+    nama: "Influenza",
+    biaya: 400000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 32,
+    kategori: "Imunisasi (Rumah Vaksin)",
+    nama: "Thypoid",
+    biaya: 400000,
 
     is_created_by_user: false,
   },
@@ -59,7 +107,7 @@ export const data = [
     _id: 8,
     kategori: "Listrik",
     nama: "450 VA",
-    biaya: 0,
+    biaya: 415,
 
     is_created_by_user: false,
   },
@@ -99,7 +147,7 @@ export const data = [
     _id: 13,
     kategori: "Listrik",
     nama: ">6600 VA",
-    biaya: 1445,
+    biaya: 1670,
 
     is_created_by_user: false,
   },
@@ -123,31 +171,199 @@ export const data = [
     _id: 16,
     kategori: "BPJS",
     nama: "Kelas 3",
+    biaya: 35000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 22,
+    kategori: "BPJS",
+    nama: "PBI",
     biaya: 42000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 33,
+    kategori: "Rawat Inap (Hari)",
+    nama: "VIP",
+    biaya: 350000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 34,
+    kategori: "Rawat Inap (Hari)",
+    nama: "Kelas 1",
+    biaya: 200000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 35,
+    kategori: "Rawat Inap (Hari)",
+    nama: "Kelas 2",
+    biaya: 120000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 36,
+    kategori: "Rawat Inap (Hari)",
+    nama: "Kelas 3",
+    biaya: 75000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 37,
+    kategori: "Visit Dokter (Hari)",
+    nama: "Rawat Inap",
+    biaya: 75000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 38,
+    kategori: "KB",
+    nama: "IUD/Implant",
+    biaya: 105000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 39,
+    kategori: "KB",
+    nama: "Suntik",
+    biaya: 20000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 40,
+    kategori: "KB",
+    nama: "MOP",
+    biaya: 370000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 41,
+    kategori: "Rawat Jalan",
+    nama: "Cek Darah",
+    biaya: 20000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 42,
+    kategori: "Rawat Jalan",
+    nama: "Kolestrol",
+    biaya: 60000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 43,
+    kategori: "Rawat Jalan",
+    nama: "Trigliserida",
+    biaya: 50000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 44,
+    kategori: "Rawat Jalan",
+    nama: "Pap smear",
+    biaya: 125000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 50,
+    kategori: "Rawat Jalan",
+    nama: "Gigi",
+    biaya: 206000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 51,
+    kategori: "Rawat Jalan",
+    nama: "Protesa Gigi",
+    biaya: 1100000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 45,
+    kategori: "Rawat Jalan",
+    nama: "Kehamilan K1/K2/K3/K4",
+    biaya: 60000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 46,
+    kategori: "Persalinan",
+    nama: "Puskesmas",
+    biaya: 1000000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 47,
+    kategori: "Persalinan",
+    nama: "FKTP selain Puskesmas",
+    biaya: 1200000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 48,
+    kategori: "Persalinan",
+    nama: "Emergensi",
+    biaya: 1500000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 49,
+    kategori: "Persalinan",
+    nama: "Bedah Caesar",
+    biaya: 7733000,
+
+    is_created_by_user: false,
+  },
+    {
+    _id: 23,
+    kategori: "Pendidikan (BOS)",
+    nama: "PAUD",
+    biaya: 710000,
 
     is_created_by_user: false,
   },
   {
     _id: 17,
     kategori: "Pendidikan (BOS)",
-    nama: "SD",
-    biaya: 950000,
+    nama: "SD/MI",
+    biaya: 1070000,
 
     is_created_by_user: false,
   },
   {
     _id: 18,
     kategori: "Pendidikan (BOS)",
-    nama: "SMP",
-    biaya: 1190000,
+    nama: "SMP/MTS",
+    biaya: 1300000,
 
     is_created_by_user: false,
   },
   {
     _id: 19,
     kategori: "Pendidikan (BOS)",
-    nama: "SMA",
-    biaya: 1620000,
+    nama: "SMA/MA",
+    biaya: 1780000,
 
     is_created_by_user: false,
   },
@@ -155,7 +371,7 @@ export const data = [
     _id: 20,
     kategori: "Pendidikan (BOS)",
     nama: "SMK",
-    biaya: 1730000,
+    biaya: 1900000,
 
     is_created_by_user: false,
   },
@@ -163,7 +379,215 @@ export const data = [
     _id: 21,
     kategori: "Pendidikan (BOS)",
     nama: "SLB",
-    biaya: 3750000,
+    biaya: 4150000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 24,
+    kategori: "Pendidikan (BOS)",
+    nama: "Paket A",
+    biaya: 1300000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 25,
+    kategori: "Pendidikan (BOS)",
+    nama: "Paket B",
+    biaya: 1500000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 26,
+    kategori: "Pendidikan (BOS)",
+    nama: "Paket C",
+    biaya: 1800000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 52,
+    kategori: "Pendidikan (PIP)",
+    nama: "SD/MI",
+    biaya: 225000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 53,
+    kategori: "Pendidikan (PIP)",
+    nama: "SMP/MTS",
+    biaya: 375000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 54,
+    kategori: "Pendidikan (PIP)",
+    nama: "SMA/MA/SMK",
+    biaya: 500000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 55,
+    kategori: "Pendidikan (PIP)",
+    nama: "PT",
+    biaya: 4800000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 56,
+    kategori: "Air (PDAM)",
+    nama: "0-10 m3",
+    biaya: 4140,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 57,
+    kategori: "Air (PDAM)",
+    nama: "11-20 m3",
+    biaya: 5070,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 58,
+    kategori: "Air (PDAM)",
+    nama: "21-30 m3",
+    biaya: 6000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 59,
+    kategori: "Air (PDAM)",
+    nama: ">20 m3",
+    biaya: 6930,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 60,
+    kategori: "Jasa Lembaga Keuangan",
+    nama: "Mandiri",
+    biaya: 12500,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 61,
+    kategori: "Jasa Lembaga Keuangan",
+    nama: "BCA",
+    biaya: 15000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 62,
+    kategori: "Jasa Lembaga Keuangan",
+    nama: "BNI",
+    biaya: 11000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 63,
+    kategori: "Jasa Lembaga Keuangan",
+    nama: "BRI",
+    biaya: 12000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 64,
+    kategori: "Jasa Lembaga Keuangan",
+    nama: "BSI",
+    biaya: 10000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 65,
+    kategori: "Bantuan",
+    nama: "BPNT (Perbulan)",
+    biaya: 200000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 66,
+    kategori: "Bantuan",
+    nama: "BLT Dana Desa (Perbulan)",
+    biaya: 300000,
+
+    is_created_by_user: false,
+  },
+  {
+    _id: 67,
+    kategori: "Bantuan",
+    nama: "Kartu Prakerja",
+    biaya: 4200000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 68,
+    kategori: "PKH",
+    nama: "Balita, Ibu Hamil/Menyusui (Pertahun)",
+    biaya: 3000000,
+
+    is_created_by_user: false,
+  },
+    {
+    _id: 69,
+    kategori: "PKH",
+    nama: "Anak Usia Dini (Pertahun)",
+    biaya: 3000000,
+
+    is_created_by_user: false,
+  },
+    {
+    _id: 70,
+    kategori: "PKH",
+    nama: "Anak SD/MI (Pertahun)",
+    biaya: 900000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 71,
+    kategori: "PKH",
+    nama: "Anak SMP/MTS (Pertahun)",
+    biaya: 1500000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 72,
+    kategori: "PKH",
+    nama: "Anak SMA/MA (Pertahun)",
+    biaya: 2000000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 73,
+    kategori: "PKH",
+    nama: "Disabilitas Berat (Pertahun",
+    biaya: 2400000,
+
+    is_created_by_user: false,
+  },
+   {
+    _id: 74,
+    kategori: "PKH",
+    nama: "Lansia >70 tahun (Pertahun)",
+    biaya: 2400000,
 
     is_created_by_user: false,
   },
